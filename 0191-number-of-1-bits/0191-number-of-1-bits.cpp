426d7bb3451ddf3b9ad3d8e4222b39e00ddae7fe
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2023-10-31 21:12:09 +0530

    Time: 2 ms (36.4%), Space: 6.5 MB (19.55%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
+        auto temp = n;
+        int ct = 0;
+        while(temp)
+        {
+            if(temp & 1) ct++;
+            temp = temp >> 1;
+        }
+        return ct;
     }
 };
