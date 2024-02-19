1b8f7d4ed5bdf81b4d75774a41d3332bce303809
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-02-19 17:01:50 -0500

    Time: 157 ms (5.13%), Space: 91.3 MB (5.04%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -4,46 +4,38 @@ public:
     {
         return (0 <= i && i < m) && (0 <= j && j < n);
     }
-
-    int bfs(queue<vector<int>>& q, vector<vector<int>>& grid, vector<vector<int>>& vis, int m, int n)
-    {
-        int ct = 0, i, j, k;
-        vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while(!q.empty())
-        {
-            vector<int> temp = q.front();
-            i = temp[0], j = temp[1];
-            q.pop();
-            if(isValid(i, j, m, n) && !vis[i][j] && grid[i][j])
-            {
-                vis[i][j] = 1;
-                ct++;
-                for(k = 0; k < 4; k++)
-                {
-                    q.push({i + dirs[k][0], j + dirs[k][1]});
-                }
-            }
-        }
-        return ct;
-    }
-
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size(), i, j;
+        int mx = 0, ct = 0;
         vector<vector<int>> vis(m, vector<int>(n, 0));
-        int ans = 0, ct;
-        queue<vector<int>> q;
+        queue<pair<int, int>> q;
         for(i = 0; i < m; i++)
         {
             for(j = 0; j < n; j++)
             {
-                if(!vis[i][j] && grid[i][j])
+                if(!vis[i][j] && grid[i][j] == 1)
                 {
                     q.push({i, j});
-                    ct = bfs(q, grid, vis, m, n);
-                    ans = max(ans, ct);
+                    ct = 0;
+                    while(!q.empty())
+                    {
+                        vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+                        auto [x, y] = q.front();
+                        q.pop();    
+                        if(isValid(x, y, m, n) && !vis[x][y] && grid[x][y] == 1)
+                        {
+                            ct++;
+                            vis[x][y] = 1;
+                            for(int k = 0; k < 4; k++)
+                            {
+                                q.push({x + dirs[k][0], y + dirs[k][1]});
+                            }
+                        }
+                    }
+                    mx = max(mx, ct);
                 }
             }
         }
-        return ans;
+        return mx;       
     }
 };
