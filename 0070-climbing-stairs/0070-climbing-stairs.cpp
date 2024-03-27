d7ed3339529742df73374d0652b73c30331aa322
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-03-27 00:58:28 -0400

    Space optimization to 0070-climbing-stairs.cpp

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 1) return 1;
-        vector<int> dp(n+1, 0);
-        int i;
-        dp[1] = 1;
-        dp[2] = 2;
-        for(i = 3; i <= n; i++)
+        if(n <= 1) return 1;
+        int a, b, c;
+        a = 1;
+        b = 1;
+        for(int i = 2; i <= n; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return c;
     }
 };
