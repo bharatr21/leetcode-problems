b624425830174df040deab4aa2f2cc48ff89178d
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-04-12 18:35:30 -0400

    Update 0242-valid-anagram.cpp to account for edge cases

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int m = s.size(), n = t.size();
-        if(m != n) return false;
-        // Put it into two character frequency maps, then check if they are same
-        unordered_map<char, int> ms, mt;
-        for(char ch: s) ms[ch]++;
-        for(char ch: t) mt[ch]++;
-        for(auto u: ms)
+        unordered_map<char, int> fs, ft;
+        for(char c: s) fs[c]++;
+        for(char c: t) ft[c]++;
+        for(auto u: fs)
         {
-            if(mt[u.first] != u.second) return false;
+            if(ft.find(u.first) == ft.end() || ft[u.first] != u.second) return false;
+        }
+        for(auto u: ft)
+        {
+            if(fs.find(u.first) == fs.end() || fs[u.first] != u.second) return false;
         }
         return true;
     }
