384ba41ae10c99481a0363f7e3387df7a59b529e
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-04-29 14:23:37 -0400

    Time: 2 ms(43.85%), Space: 11.9 MB (43.5%) - Bharat

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,8 +12,8 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if((p && !q) || (!p && q)) return false;
-        else if(!p && !q) return true;
-        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        if(!p && !q) return true;
+        else if(!p && q || p && !q) return false;
+        return ((p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
