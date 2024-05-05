4a22357b307627734d50e8611ee2a04fd2410432
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-05-05 01:33:09 -0400

    Time: 203 ms (13.99%), Space: 144.8 MB (62.75%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Note: Meta SWE E5 2024 - Phone Screen 
 class Solution {
 public:
     bool isLeaf(TreeNode* node) {
