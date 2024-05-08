823ba5f4fef79371cec65876f16e0e8c4e01babb
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-05-07 23:35:15 -0400

    Time: 9 ms (98.10%), Space: 17.2 MB (83.81%) - LeetHub

--- 2473-minimum-cost-to-buy-apples/2473-minimum-cost-to-buy-apples.cpp
+++ 2473-minimum-cost-to-buy-apples/2473-minimum-cost-to-buy-apples.cpp
@@ -4,59 +4,47 @@ public:
         // Store the graph as a list of lists
         // The rows represent the cities (vertices)
         // The columns store an adjacency list of road, cost pairs (edge, weight)
-        vector<vector<pair<int, int>>> graph(n, vector<pair<int, int>>());
-
+        vector<vector<pair<int, int>>> graph(n);
+        
         // Add each road to the graph using adjacency lists
-        // Store each city at graph[city - 1]
+        // Store each city at `graph[city - 1]`
         for (auto& road : roads) {
             int city_a = road[0] - 1, city_b = road[1] - 1, cost = road[2];
             graph[city_a].push_back({city_b, cost});
             graph[city_b].push_back({city_a, cost});
         }
 
-        // Find the minimum cost to buy an apple starting in each city
-        vector<long long> result(n);
-        for (int startCity = 0; startCity < n; startCity++) {
-            result[startCity] = shortestPath(startCity, graph, appleCost, k, n);
+        // Store the cost to buy an apple in each city 
+        // without traveling in the result
+        vector<long long> result(appleCost.begin(), appleCost.end());
+        
+        // Initialize the heap (priority queue) with each starting city
+        // Each element of the heap is a pair with the cost and city
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        for (int start_city = 0; start_city < appleCost.size(); start_city++) {
+            heap.push({appleCost[start_city], start_city});
         }
-
-        return result;
-    }
-
-private:
-    // Finds the minimum cost to buy an apple from the start city
-    long long shortestPath(int startCity, vector<vector<pair<int, int>>> graph,
-                           vector<int>& appleCost, int k, int n) {
-        // Stores the travel cost reach each city from the start city
-        vector<int> travelCosts(n, INT_MAX);
-        travelCosts[startCity] = 0;
-
-        // Initialize the heap (priority queue) with the starting city
-        // Each element of the heap is an array with the cost and city
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> heap;
-        heap.push({0, startCity});
-        long long minCost = INT_MAX;
-
+        
+        // Find the minimum cost to buy an apple starting in each city
         while (!heap.empty()) {
-            // Remove the city with the minimum cost from the top of the heap
-            auto current = heap.top();
+            auto [total_cost, curr_city] = heap.top();
             heap.pop();
-            int travelCost = current[0];
-            int currCity = current[1];
-
-            // Update the min cost if the curr city has a smaller total cost
-            minCost = min(minCost, 
-                    static_cast<long long>(appleCost[currCity]) + (k + 1) * travelCost);
-
-            // Add each neighboring city to the heap if an apple is cheaper
-            for (auto& [neighbor, cost] : graph[currCity]) {
-                int nextCost = travelCost + cost;
-                if (nextCost < travelCosts[neighbor]) {
-                    travelCosts[neighbor] = nextCost;
-                    heap.push({nextCost, neighbor});
+            
+            // If we have already found a path to buy an apple
+            // for cheaper than the local apple cost, skip this city
+            if (result[curr_city] < total_cost)
+                continue;
+            
+            // Add each neighboring city to the heap if it is cheaper to
+            // start there, travel to the current city and buy an apple 
+            // than buy in the neighboring city
+            for (auto [neighbor, cost] : graph[curr_city]) {
+                if (result[neighbor] > result[curr_city] + (k + 1) * cost) {
+                    result[neighbor] = result[curr_city] + (k + 1) * cost;
+                    heap.push({result[neighbor], neighbor});
                 }
             }
         }
-        return minCost;
+        return result;
     }
 };
