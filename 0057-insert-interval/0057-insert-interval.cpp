8f0e0cca031fa787ecb90472d622203264484c59
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-05-19 01:27:50 -0400

    Time: 18 ms (15.04%), Space: 20.7 MB (24.42%) - Bharat

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> res;
         intervals.push_back(newInterval);
-        int n = intervals.size(), i;
         sort(intervals.begin(), intervals.end());
-        res.push_back(intervals[0]);
-        if(n == 1) return res;
-        for(i = 1; i < n; i++)
-        {
-            int start = intervals[i][0], end = intervals[i][1];
-            if(res.back().back() < start || res.back()[0] > end)
-            {
-                res.push_back(intervals[i]);
-            }
-            else
-            {
-                res.back().back() = max(res.back().back(), end);
+        vector<vector<int>> res = {intervals[0]};
+        int n = intervals.size();
+        for(int i = 1; i < n; i++) {
+            int st = res.back()[0], en = res.back()[1];
+            int a = intervals[i][0], b = intervals[i][1];
+            if(b < st || a > en) res.push_back({a, b});
+            else {
+                res.back()[0] = min(a, st);
+                res.back().back() = max(b, en);
             }
         }
         return res;
