7bd8942033b8a99b51f63060f80324b32c0ff073
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-06-01 20:16:22 -0400

    Time: 19 ms (96.84%), Space: 19.9 MB (36.84%) - LeetHub

--- 2071-longest-common-subsequence-between-sorted-arrays/2071-longest-common-subsequence-between-sorted-arrays.cpp
+++ 2071-longest-common-subsequence-between-sorted-arrays/2071-longest-common-subsequence-between-sorted-arrays.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+    vector<int> lcs(vector<int>& a1, vector<int>& a2) {
+        vector<int> longest;
+        int m = a1.size(), n = a2.size(), sz = min(m, n);
+        int i = 0, j = 0;
+        while(i < m && j < n) {
+            if(a1[i] < a2[j]) i++;
+            else if(a1[i] > a2[j]) j++;
+            else {longest.push_back(a2[j]); i++; j++;}
+        }
+        return longest;
+    }
+
     vector<int> longestCommonSubsequence(vector<vector<int>>& arrays) {
-        unordered_map<int, int> mp;
+        int n = arrays.size(), i;
         vector<int> res;
-        int n = arrays.size();
-        for(vector<int> array: arrays)
+        res = lcs(arrays[0], arrays[1]);
+        for(i = 2; i < n; i++)
         {
-            for(int el: array)
-            {
-                mp[el]++;
-            }
+            res = lcs(res, arrays[i]);
         }
-        for(auto u: mp)
-        {
-            if(u.second == n) res.push_back(u.first);
-        }
-        sort(res.begin(), res.end());
         return res;
     }
 };
