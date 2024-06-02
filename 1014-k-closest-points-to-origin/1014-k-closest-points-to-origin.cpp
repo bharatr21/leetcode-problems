d31468834eddeb621ed59530aae0a990a582c36c
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-06-02 15:04:01 -0400

    Time: 212 ms (21.88%), Space: 87.2 MB (18.54%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,14 +1,31 @@
+class Compare {
+    public:
+    bool operator()(vector<int>& p1, vector<int>& p2) {
+        return p1[0] < p2[0];
+    }
+};
+
 class Solution {
 public:
-    static bool cmp(vector<int>& p1, vector<int>& p2) {
-        return (p1[0] * p1[0] + p1[1] * p1[1] < p2[0] * p2[0] + p2[1] * p2[1]);
+    int dist(vector<int>& point) {
+        return (point[0] * point[0] + point[1] * point[1]);
     }
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>> res;
-        int n = points.size(), i;
-        sort(points.begin(), points.end(), cmp);
-        for(i = 0; i < k; i++) {
-            res.push_back(points[i]);
+        priority_queue<vector<int>, vector<vector<int>>, Compare> pq;
+        for(vector<int> point: points) {
+            pq.push({dist(point), point[0], point[1]});
+            if(pq.size() > k) {
+                auto u = pq.top();
+                pq.pop();
+            }            
+        }
+        while(!pq.empty())
+        {
+            auto u = pq.top();
+            res.push_back({u[1], u[2]});
+            pq.pop();
         }
         return res;
     }
