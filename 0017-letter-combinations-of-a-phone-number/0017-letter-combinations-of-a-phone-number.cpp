e152dc35a838789c0d821d80339d5606b79ab3bd
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-03 22:27:50 -0400

    Time: 0 ms (100%), Space: 8.9 MB (13.74%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    void solve(string digits, unordered_map<char, string>& nmap, string& tmp, vector<string>& res)
-    {
-        if(tmp.size() == digits.size())
-        {
-            if(!tmp.empty()) res.push_back(tmp);
+    void backtrack(vector<string>& res, string tmp, string digits, unordered_map<char, vector<string>>& mp, int idx) {
+        if(idx == digits.size()) {
+            res.push_back(tmp);
             return;
         }
-        int idx = tmp.size();
-        for(char c: nmap[digits[idx]])
-        {
-            tmp += c;
-            solve(digits, nmap, tmp, res);
-            tmp.pop_back();
+        for(string d: mp[digits[idx]]) {
+            tmp += d;
+            backtrack(res, tmp, digits, mp, idx + 1);
+            tmp.erase(idx);
         }
-
-
     }
     vector<string> letterCombinations(string digits) {
-        unordered_map<char, string> nmap = {
-            {'2', "abc"},
-            {'3', "def"},
-            {'4', "ghi"},
-            {'5', "jkl"},
-            {'6', "mno"},
-            {'7', "pqrs"},
-            {'8', "tuv"},
-            {'9', "wxyz"}
-        };
-        string tmp;
+        if(digits.size() == 0) return {};
         vector<string> res;
-        solve(digits, nmap, tmp, res);
+        string tmp;
+        unordered_map<char, vector<string>> mp = {
+            {'2', {"a", "b", "c"}},
+            {'3', {"d", "e", "f"}},
+            {'4', {"g", "h", "i"}},
+            {'5', {"j", "k", "l"}},
+            {'6', {"m", "n", "o"}},
+            {'7', {"p", "q", "r", "s"}},
+            {'8', {"t", "u", "v"}},
+            {'9', {"w", "x", "y", "z"}}
+        };
+        backtrack(res, tmp, digits, mp, 0);
         return res;
     }
 };
