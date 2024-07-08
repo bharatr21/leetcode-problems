7a9091db59b56b51d1ab0bba804190afca42b18a
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-08 16:31:53 -0400

    Time: 0 ms (100%), Space: 7.4 MB (50.07%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -3,25 +3,16 @@ public:
     int findTheWinner(int n, int k) {
         // Initialize vector of N friends, labeled from 1-N
         vector<int> circle;
-        for (int i = 1; i <= n; i++) {
+        int i;
+        for (i = 1; i <= n; i++) {
             circle.push_back(i);
         }
-
-        // Maintain the index of the friend to start the count on
-        int startIndex = 0;
-
-        // Perform eliminations while there is more than 1 friend left
-        while (circle.size() > 1) {
-            // Calculate the index of the friend to be removed
-            int removalIndex = (startIndex + k - 1) % circle.size();
-
-            // Erase the friend at removalIndex
-            circle.erase(circle.begin() + removalIndex);
-
-            // Update startIndex for the next round
-            startIndex = removalIndex;
+        i = 0;
+        while(circle.size() > 1) {
+            int cur = 1;
+            i = (i + k - 1) % circle.size();
+            circle.erase(circle.begin() + i);
         }
-
         return circle.front();
     }
 };
