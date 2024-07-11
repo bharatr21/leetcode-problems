83994f47a7ea5e0d69e44fef2d6b1b1b8297616b
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-11 15:08:34 -0400

    Memoization from Recursion Solution - Bharat

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    int lcs(vector<vector<int>>& dp, string text1, string text2, int i, int j) {
+        int res;
+        if(i == 0 || j == 0) return 0;
+        else if(dp[i][j] != -1) return dp[i][j];
+        else if(text1[i-1] == text2[j-1]) res = 1 + lcs(dp, text1, text2, i-1, j-1);
+        else res = max(lcs(dp, text1, text2, i, j-1), lcs(dp, text1, text2, i-1, j));
+        return (dp[i][j] = res);
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.size(), n = text2.size(), i, j;
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        for(i = m-1; i >= 0; i--)
-        {
-            for(j = n-1; j >= 0; j--)
-            {
-                if(dp[i][j] != 0) return dp[i][j];
-                else if(text1[i] == text2[j]) {dp[i][j] = 1 + dp[i+1][j+1];}
-                else dp[i][j] = max(dp[i][j+1], dp[i+1][j]);
-            }
-        }
-        return dp[0][0];
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return lcs(dp, text1, text2, m, n);
     }
 };
