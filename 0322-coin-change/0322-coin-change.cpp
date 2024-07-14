ca75e26ec041205606e14629b0b932be96dd09c8
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-14 00:53:42 -0400

    Recursive Memoization Solution - Bharat

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, 10005);
-        int i;
-        dp[0] = 0;
-        for(i = 1; i <= amount; i++) {
+    int solve(vector<int>& dp, vector<int>& coins, int amount) {
+        int res = 10005;
+        if(amount == 0) return 0;
+        else if(amount < 0) return 10005;
+        else if(dp[amount] < 10005) return dp[amount];
+        else {
             for(int coin: coins) {
-                if(coin <= i) {
-                    dp[i] = min(dp[i], 1 + dp[i-coin]);
-                }
+                res = min(res, 1 + solve(dp, coins, amount - coin));
             }
         }
+        return (dp[amount] = res);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, 10005);
+        dp[0] = 0;
+        solve(dp, coins, amount);
         return ((dp[amount] > amount) ? -1: dp[amount]);
     }
 };
