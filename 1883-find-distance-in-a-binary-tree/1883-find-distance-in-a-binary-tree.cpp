56a99571e759edc726360fe32f9c8699340c2670
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-16 19:33:05 -0400

    Time: 28 ms (61.43%), Space: 30.1 MB (53.17%) - LeetHub

---
+++ 1883-find-distance-in-a-binary-tree/1883-find-distance-in-a-binary-tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dist(TreeNode* root, int target) {
+        int mn = 10005;
+        if(!root) return 10005;
+        else if(root->val == target) return 0;
+        else mn = min(mn, 1 + min(dist(root->left, target), dist(root->right, target)));
+        return mn;
+    }
+    TreeNode* lca(TreeNode* root, int p, int q) {
+        if(!root) return NULL;
+        else if(root->val == p || root->val == q) return root;
+        TreeNode* left = lca(root->left, p, q);
+        TreeNode* right = lca(root->right, p, q);
+        if(left && right) return root;
+        return ((left) ? left : right);
+    }
+
+    int findDistance(TreeNode* root, int p, int q) {
+        if(p == q) return 0;
+        TreeNode* node = lca(root, p, q);
+        // cout << "LCA value: " << node->val << endl;
+        int p_dist = dist(node, p);                      
+        int q_dist = dist(node, q);
+        // cout << p_dist << ' ' << q_dist << endl;
+        return (p_dist + q_dist);
+    }
+};
