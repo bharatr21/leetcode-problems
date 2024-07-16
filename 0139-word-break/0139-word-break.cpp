a2c7ba274e944a1e56af5a21d4b399c4001fe24f
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-15 21:34:41 -0400

    Time: 3 ms (87.55%), Space: 13.1 MB (60.44%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool solve(vector<int>& dp, string s, int idx, vector<string>& wordDict) {
+        // cout << idx;
+        int n = s.size();
+        // cout << ", " << n;
+        if(idx >= n) return true;
+        if(dp[idx] != -1) return dp[idx];
+        string temp = s.substr(idx);
+        // cout << ", " << temp << endl;
+        for(string word: wordDict) {
+            if(temp.rfind(word, 0) == 0) {
+                if(solve(dp, s, idx + word.size(), wordDict)) return (dp[idx] = 1);
+            }
+        }
+        return (dp[idx] = 0);
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<int> dp(n, -1);
+        return solve(dp, s, 0, wordDict);
+    }
+};
