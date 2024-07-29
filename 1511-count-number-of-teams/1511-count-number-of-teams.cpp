58b0a07ebfdba5364d2d10cecbab09d2278397cd
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-29 17:41:37 -0400

    Time: 26 ms (83.56%), Space: 12.1 MB (33.4%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int numTeams(vector<int>& rating) {
         int n = rating.size(), i, j;
-        vector<int> ratingsGreaterBefore(n, 0), ratingsGreaterAfter(n, 0),
-        ratingsLesserBefore(n, 0), ratingsLesserAfter(n, 0);
+        vector<int> ratingsGreaterAfter(n, 0), ratingsLesserAfter(n, 0);
         int res = 0;
         for(i = 0; i < n; i++) {
             int base = rating[i];
             for(j = i+1; j < n; j++) {
                 if(rating[j] < base) ratingsLesserAfter[i]++;
                 else ratingsGreaterAfter[i]++;
-            }
-            for(j = i-1; j >= 0; j--) {
-                if(rating[j] < base) ratingsLesserBefore[i]++;
-                else ratingsGreaterBefore[i]++;
             }
         }
         for(i = 0; i < n; i++) {
