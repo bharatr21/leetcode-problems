840e766ba61d862f3626b108a9c632aa89803536
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-30 01:06:37 -0400

    Time: 4 ms (18.57%), Space: 7.8 MB (6.86%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.cpp
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     bool checkString(string s) {
         int n = s.size(), mxa, mnb, i;
-        for(i = n - 1; i >= 0; i--) {
-            if(s[i] == 'a') {mxa = i; break;}
-        }
         for(i = 0; i < n; i++) {
             if(s[i] == 'b') {mnb = i; break;}
         }
-        return (mxa < mnb);
+        for(i = mnb; i < n; i++) {
+            if(s[i] == 'a') return false;
+        }
+        return true;
     }
 };
