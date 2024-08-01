7921613e32177e7c19057735761d1e8252ad940f
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-07-31 20:28:03 -0400

    Time: 8 ms (47.01%), Space: 17.4 MB (35.3%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int res = 0;
+        for(string detail: details) {
+            int age = (detail[11] - '0') * 10 + (detail[12] - '0');
+            if(age > 60) res++;
+        }
+        return res;
+    }
+};
