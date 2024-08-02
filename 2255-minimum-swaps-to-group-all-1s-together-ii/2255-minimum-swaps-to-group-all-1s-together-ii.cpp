b7c9604646dc129170d0cd994c8366d75b65173e
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-02 14:10:50 -0400

    Time: 73 ms (35.76%), Space: 83.1 MB (57.56%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,48 +1,15 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        // Calculate the minimum swaps needed to group all 1s or all 0s
-        // together
-        int op1 = minSwapsHelper(nums, 0);  // Grouping all 0s together
-        int op2 = minSwapsHelper(nums, 1);  // Grouping all 1s together
-        return min(op1, op2);
-    }
+        const int n = nums.size();
+        const int k = accumulate(nums.cbegin(), nums.cend(), 0);
+        int window_sum = accumulate(nums.cbegin(), nums.cbegin() + k, 0);
+        int swaps = k - window_sum;
 
-    // Helper function to calculate the minimum swaps required to group all
-    // `val` together
-    int minSwapsHelper(vector<int>& data, int val) {
-        int length = data.size();
-        int totalValCount = 0;
-
-        // Count the total number of `val` in the array
-        for (int i = length - 1; i >= 0; i--) {
-            if (data[i] == val) totalValCount++;
+        for (int i = 1; i < n; ++i) {
+            window_sum += nums[(i - 1 + k) % n] - nums[i - 1];
+            swaps = min(swaps, k - window_sum);
         }
-
-        // If there is no `val` or the array is full of `val`, no swaps are
-        // needed
-        if (totalValCount == 0 || totalValCount == length) return 0;
-
-        int start = 0, end = 0;
-        int maxValInWindow = 0, currentValInWindow = 0;
-
-        // Initial window setup: count the number of `val` in the first window
-        // of size `totalValCount`
-        while (end < totalValCount) {
-            if (data[end++] == val) currentValInWindow++;
-        }
-        maxValInWindow = max(maxValInWindow, currentValInWindow);
-
-        // Slide the window across the array to find the maximum number of
-        // `val` in any window
-        while (end < length) {
-            if (data[start++] == val) currentValInWindow--;
-            if (data[end++] == val) currentValInWindow++;
-            maxValInWindow = max(maxValInWindow, currentValInWindow);
-        }
-
-        // Minimum swaps are the total `val` minus the maximum found in any
-        // window
-        return totalValCount - maxValInWindow;
+        return swaps;
     }
 };
