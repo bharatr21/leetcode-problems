f082ba21aebbcafa1d1c458508fc09d82857258c
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-10 14:31:18 -0400

    Time: 4 ms (58.39%), Space: 15.2 MB (25.36%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
@@ -13,19 +13,19 @@ class Solution {
 public:
     vector<vector<int>> verticalOrder(TreeNode* root) {
         vector<vector<int>> res;
-        map<int, vector<int>> lvls;
+        map<int, vector<int>> levels;
         if(!root) return res;
         queue<pair<TreeNode*, int>> q;
         q.push({root, 0});
         while(!q.empty()) {
             auto [node, level] = q.front();
             q.pop();
-            lvls[level].push_back(node->val);
+            levels[level].push_back(node->val);
             if(node->left) q.push({node->left, level - 1});
             if(node->right) q.push({node->right, level + 1});
         }
-        for(auto u: lvls) {
-            res.push_back(u.second);
+        for(auto it: levels) {
+            res.push_back(it.second);
         }
         return res;
     }
