49c97409f262138f7555a23998d8bb78d96f99ad
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-12 18:52:29 -0400

    Time: 100 ms (78.88%), Space: 99.4 MB (23.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    bool canFind(vector<vector<int>>& dp, vector<int>& nums, int idx, int target) {
-        int n = nums.size();
-        if(target == 0) return true;
-        else if(target < 0 || idx >= n) return false;
-        else if(dp[idx][target] != -1) return dp[idx][target];
-        bool pick = canFind(dp, nums, idx + 1, target - nums[idx]);
-        bool notPick = canFind(dp, nums, idx + 1, target);
-        return (dp[idx][target] = (pick || notPick));
-    }
-
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
