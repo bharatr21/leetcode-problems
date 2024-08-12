2575d2f6770f0c4ca52f1999b18067036b77fe9e
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-12 18:51:50 -0400

    Time: 92 ms (81.25%), Space: 99.6 MB (20.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -15,7 +15,17 @@ public:
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum % 2) return false;
         int target = sum / 2;
-        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
-        return canFind(dp, nums, 0, target);
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+        for(int i = 1; i <= target; i++) dp[n][i] = 0;
+        for(int idx = n - 1; idx >= 0; idx--) {
+            for(int j = 1; j <= target; j++)
+            if(nums[idx] <= j) {
+                dp[idx][j] = (dp[idx+1][j - nums[idx]] || dp[idx+1][j]);
+            }
+            else
+                dp[idx][j] = dp[idx+1][j];
+        }        
+        return dp[0][target];
     }
 };
