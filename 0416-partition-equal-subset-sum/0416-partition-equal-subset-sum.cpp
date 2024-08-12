d374042453522ae5ee5d2bfa93580915882a21eb
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-12 18:59:23 -0400

    Time: 77 ms (86.16%), Space: 14.1 MB (84.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -5,17 +5,21 @@ public:
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum % 2) return false;
         int target = sum / 2;
-        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
-        for(int i = 0; i <= n; i++) dp[i][0] = 1;
-        for(int i = 1; i <= target; i++) dp[n][i] = 0;
+        // vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        vector<int> cur(target+1, 0), next(target+1, 0);
+        for(int i = 0; i <= n; i++) cur[0] = 1;
+        for(int i = 1; i <= target; i++) next[i] = 0;
         for(int idx = n - 1; idx >= 0; idx--) {
-            for(int j = 1; j <= target; j++)
-            if(nums[idx] <= j) {
-                dp[idx][j] = (dp[idx+1][j - nums[idx]] || dp[idx+1][j]);
+            for(int j = 1; j <= target; j++) {
+                if(nums[idx] <= j) {
+                    cur[j] = (next[j - nums[idx]] || next[j]);
+                }
+                else {
+                    cur[j] = next[j];
+                }
             }
-            else
-                dp[idx][j] = dp[idx+1][j];
+            next = cur;
         }        
-        return dp[0][target];
+        return cur[target];
     }
 };
