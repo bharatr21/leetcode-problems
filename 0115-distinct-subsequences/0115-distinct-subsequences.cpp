93d05ca7e83d674080fc869724c882e8f9232119
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-16 23:48:01 -0400

    Time: 22 ms (44.34%), Space: 20.1 MB (53.73%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& dp, string s, int sidx, string t, int tidx) {
-        int m = s.size(), n = t.size();
-        if(sidx == m || tidx == n) {
-            return ((tidx == n) ? 1 : 0);
-        }
-        if(dp[sidx][tidx] != -1) return dp[sidx][tidx];
-        int res = 0;
-        //Consider existing set as match + existing set as mismatch
-        if(s[sidx] == t[tidx]) res += solve(dp, s, sidx + 1, t, tidx + 1);
-        res += solve(dp, s, sidx + 1, t, tidx);
-        return (dp[sidx][tidx] = res);
-    }
-
     int numDistinct(string s, string t) {
         int m = s.size(), n = t.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return solve(dp, s, 0, t, 0);
+        vector<vector<unsigned int>> dp(m+1, vector<unsigned int>(n+1, 0));
+        for(int j = 0; j <= n; j++) dp[m][j] = 0;
+        for(int i = 0; i <= m; i++) dp[i][n] = 1;
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                dp[i][j] = dp[i+1][j];
+                if(s[i] == t[j]) dp[i][j] += dp[i+1][j+1];
+            }
+        }
+        return dp[0][0];
     }
 };
