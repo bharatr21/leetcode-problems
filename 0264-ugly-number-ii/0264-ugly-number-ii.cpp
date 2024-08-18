bb1e6b136ddb43c83b26b17d650a1a536c4e99e6
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-18 01:09:19 -0400

    Time: 84 ms (20.81%), Space: 34.3 MB (11.23%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        set<long> res;
+        priority_queue<long, vector<long>, greater<long>> pq;
+        unordered_set<long> seen;
         long cur;
-        res.insert(1);
+        pq.push(1);
+        seen.insert(1);
         for(int i = 0; i < n; i++) {
-            cur = *res.begin();
-            res.erase(cur);
-            res.insert(cur * 2);
-            res.insert(cur * 3);
-            res.insert(cur * 5);
+            cur = pq.top();
+            pq.pop();
+            if(!seen.count(cur * 2)) {pq.push(cur * 2); seen.insert(cur * 2); }
+            if(!seen.count(cur * 3)) {pq.push(cur * 3); seen.insert(cur * 3); }
+            if(!seen.count(cur * 5)) {pq.push(cur * 5); seen.insert(cur * 5); }
         }
         return (int)cur;
     }
