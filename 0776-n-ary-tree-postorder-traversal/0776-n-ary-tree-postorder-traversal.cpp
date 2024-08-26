6f5ff78da58a9df592fa35d05355bce275cfff7c
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-26 15:31:25 -0400

    Time: 15 ms (38.63%), Space: 15.3 MB (49.47%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -20,18 +20,30 @@ public:
 
 class Solution {
 public:
-    void postN(Node* root, vector<int>& res) {
-        if(!root) return;
-        for(auto el: root->children) {
-            postN(el, res);
-        }
-        res.push_back(root->val);
-    }
-
     vector<int> postorder(Node* root) {
         vector<int> res;
-        if(!root) return res;
-        postN(root, res);
+        stack<pair<Node*, int>> st;
+        Node* curr = NULL;
+        curr = root;
+        while(curr != NULL || st.size()){
+            if(curr == NULL){
+                auto parent = st.top().first;
+                auto childNum = st.top().second;
+                curr = parent -> children[childNum];
+                st.pop();
+                if(childNum > 0 ){
+                    st.push({parent, childNum-1});
+                }
+            } else{
+                res.push_back(curr -> val);
+                if(curr -> children.size() > 1){
+                    st.push({curr, curr -> children.size()-2});
+                }
+                curr = curr -> children.size() ?
+                 curr -> children[curr -> children.size()-1] : NULL;
+            }
+        }
+        reverse(res.begin(), res.end());
         return res;
     }
 };
