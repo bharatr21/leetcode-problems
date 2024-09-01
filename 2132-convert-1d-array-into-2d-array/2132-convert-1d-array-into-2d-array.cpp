1dd8cff6c9f0bc6a3317ec8e5c5fce71d2f93bb3
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-08-31 21:45:31 -0400

    Time: 67 ms (95.32%), Space: 88.1 MB (73.42%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -4,10 +4,8 @@ public:
         int sz = original.size();
         if(sz != m * n) return {};
         vector<vector<int>> res(m, vector<int>(n, 0));
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                res[i][j] = original[i * n + j];
-            }
+        for(int i = 0; i < sz; i++) {
+                res[i / n][i % n] = original[i];
         }
         return res;
     }
