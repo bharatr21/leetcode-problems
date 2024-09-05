b8c90feb06f578b2b64972ab29a3d1342a6c7197
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-09-04 20:55:50 -0400

    Time: 104 ms (33.58%), Space: 126.4 MB (26.72%) - LeetHub

---
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        vector<int> res;
+        int m = rolls.size();
+        int asum = accumulate(rolls.begin(), rolls.end(), 0);
+        int miss = mean * (m + n) - asum;
+        if(miss > 6 * n || miss < n) {
+            cout << miss << endl;
+            return res;
+        }
+        while(miss) {
+            int rem_size = n - res.size();
+            if(miss/rem_size < 6) {res.push_back(miss / rem_size); miss -= miss/rem_size;}
+            else {res.push_back(6); miss -= 6;}
+        }
+        return res;
+    }
+};
