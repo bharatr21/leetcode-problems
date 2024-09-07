b64d04b8f9c6810f4390b5f488485a59457a3ebb
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-09-07 01:52:43 -0400

    Time: 22 ms (26.24%), Space: 20.4 MB (24.31%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,36 +1,25 @@
-class Comparator {
-    public:
-        unordered_map<string, int>& out;
-        Comparator(unordered_map<string, int>& outdeg): out(outdeg) {};
-        bool operator() (string s1, string s2) const {
-            return ((out[s1] == out[s2]) ? s1 < s2 : out[s1] > out[s2]);
-        }
-};
 class Solution {
 public:
-    void dfs(string src, unordered_map<string, vector<string>>& adj, vector<string>& res, set<pair<string, string>>& vis) {
+    void dfs(string src, unordered_map<string, vector<string>>& adj, vector<string>& res) {
         while(!adj[src].empty()) {
             auto dst = adj[src].back();
             adj[src].pop_back();
-            dfs(dst, adj, res, vis); 
+            dfs(dst, adj, res); 
         }
         res.push_back(src);
     }
+
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         vector<string> res;
         unordered_map<string, vector<string>> adj;
-        set<pair<string, string>> vis;
-        unordered_map<string, int> indeg, outdeg;
         for(vector<string> v: tickets) {
             string src = v[0], dst = v[1];
             adj[src].push_back(dst);
-            indeg[dst]++;
-            outdeg[src]++;
         }
         for(auto it: adj) {
             sort(adj[it.first].begin(), adj[it.first].end(), greater<string>());
         }
-        dfs("JFK", adj, res, vis);
+        dfs("JFK", adj, res);
         reverse(res.begin(), res.end());
         return res;
     }
