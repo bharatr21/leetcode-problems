fbb4b2f0c44bfc5c1d7303335018d6d040ca2de8
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-09-14 02:31:08 -0400

    Time: 4 ms (94.14%), Space: 13.9 MB (35.89%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -3,12 +3,13 @@ public:
     string convert(string s, int numRows) {
         string res;
         int n = numRows, i = 0;
+        if(n == 1) return s;
         vector<string> v(n);
         int sgn = -1;
         for(char ch: s) {
             if(i == 0 || i == n - 1) sgn *= -1;
             v[i] += ch;
-            i = (i + sgn * 1) % n;
+            i = (i + sgn * 1);
         }
         for(string els: v) {
             res += els;
