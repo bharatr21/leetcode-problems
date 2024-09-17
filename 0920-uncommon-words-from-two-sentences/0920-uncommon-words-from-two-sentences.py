37ef39d5783bf1aa70c3544a57515f5bddcf5070
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-09-17 11:31:51 -0400

    Time: 43 ms (8.39%), Space: 16.7 MB (5.76%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
@@ -2,8 +2,6 @@ from collections import Counter
 
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        words1 = set(s1.split())
         words1c = dict(Counter(s1.split()))
-        words2 = set(s2.split())
         words2c = dict(Counter(s2.split()))
-        return [s for s in list(words1.difference(words2) | words2.difference(words1)) if words1c.get(s, 0) == 1 or words2c.get(s, 0) == 1]
+        return [s for s in list(set(words1c.keys()).difference(set(words2c.keys())) | set(words2c.keys()).difference(set(words1c.keys()))) if words1c.get(s, 0) == 1 or words2c.get(s, 0) == 1]
