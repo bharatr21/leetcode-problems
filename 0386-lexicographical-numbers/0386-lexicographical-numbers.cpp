60abbb8b0220c05e4020797411461b3ea134e7a7
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-09-21 12:21:35 -0400

    Time: 9 ms (70.67%), Space: 12.5 MB (91.06%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
-    static bool cmp(int a, int b) {
-        return to_string(a) < to_string(b);
-    }
     vector<int> lexicalOrder(int n) {
-        vector<int> res;
-        for(int i = 1; i <= n; i++) res.push_back(i);
-        sort(res.begin(), res.end(), cmp);
-        return res;
+        vector<int> lexicographicalNumbers;
+        int currentNumber = 1;
+
+        // Generate numbers from 1 to n
+        for (int i = 0; i < n; ++i) {
+            lexicographicalNumbers.push_back(currentNumber);
+
+            // If multiplying the current number by 10 is within the limit, do
+            // it
+            if (currentNumber * 10 <= n) {
+                currentNumber *= 10;
+            } else {
+                // Adjust the current number by moving up one digit
+                while (currentNumber % 10 == 9 || currentNumber >= n) {
+                    currentNumber /= 10;  // Remove the last digit
+                }
+                currentNumber += 1;  // Increment the number
+            }
+        }
+
+        return lexicographicalNumbers;
     }
 };
