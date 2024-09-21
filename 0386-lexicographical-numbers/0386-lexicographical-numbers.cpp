77d3fdf627918d1553853cbccb7532de2671e64a
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-09-21 12:19:00 -0400

    Time: 471 ms (5.05%), Space: 17.1 MB (33.66%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
+    static bool cmp(int a, int b) {
+        return to_string(a) < to_string(b);
+    }
     vector<int> lexicalOrder(int n) {
-        vector<string> tmp;
         vector<int> res;
-        for(int i = 1; i <= n; i++) tmp.push_back(to_string(i));
-        sort(tmp.begin(), tmp.end());
-        for(auto el: tmp) res.push_back(stoi(el));
+        for(int i = 1; i <= n; i++) res.push_back(i);
+        sort(res.begin(), res.end(), cmp);
         return res;
     }
 };
