29d884c6cef4fcbde6a539593d759149c7f57b59
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-10-05 18:20:03 -0400

    Time: 7 ms (32.04%), Space: 9.5 MB (63.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,11 +3,28 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1, 0);
-        dp[n] = 0;
-        dp[n-1] = nums[n-1];
-        for(int i = n-2; i >= 0; i--) {
-            dp[i] = max(dp[i+2] + nums[i], dp[i+1]);
+        vector<pair<int, int>> chosen;
+        if(n <= 2) return *max_element(nums.begin(), nums.end());
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < n; i++) {
+            if(dp[i-2] + nums[i] > dp[i-1]) {
+                dp[i] = dp[i-2] + nums[i];
+            } else {
+                dp[i] = dp[i-1];
+            }
         }
-        return dp[0];
+        bool skip = false;
+        for(int i = n - 1; i >= 2; i--) {
+            if(skip) skip = false;
+            else if(dp[i-2] + nums[i] > dp[i-1]) {
+                chosen.push_back({i, nums[i]});
+                skip = true;
+            }
+        }
+        if(skip) chosen.push_back({0, nums[0]});
+        else chosen.push_back({1, nums[1]});
+        for(auto el: chosen) cout << "(" << el.first << ", " << el.second << ")" << endl;
+        return dp[n-1];
     }
 };
