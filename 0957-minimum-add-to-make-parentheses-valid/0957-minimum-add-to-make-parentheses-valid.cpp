a624351cb0044277989e2854e609d526fddefda6
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-10-08 22:45:55 -0400

    Time: 2 ms (44.73%), Space: 7.9 MB (32.94%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(char ch: s)
+        {
+            if(ch == '(') st.push(ch);
+            else if(ch == ')')
+            {
+                if(!st.empty() && st.top() == '(') st.pop();
+                else st.push(ch);
+            }
+        }
+        return st.size();
+    }
+};
