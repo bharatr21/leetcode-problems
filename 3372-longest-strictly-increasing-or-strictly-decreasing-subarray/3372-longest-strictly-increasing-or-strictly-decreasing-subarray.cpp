bce3979f62b5d321b833b8ba280bce96eac27683
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-02-03 13:45:28 -0500

    Time: 0 ms (100%), Space: 27.7 MB (89.71%) - LeetHub

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums, int st, int en) {
+        bool inc = false, dec = false;
+        if(en == st) return 1;
+        if(en >= st + 1) {
+            if(nums[st+1] > nums[st]) inc = true;
+            else if(nums[st+1] < nums[st]) dec = true;
+            else return false;
+        }
+        for(int i = st+1; i < en; i++) {
+            if((dec && (nums[i] <= nums[i+1])) || (inc && (nums[i] >= nums[i+1]))) return false;
+        }
+        return true;
+    }
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int n = nums.size(), mx = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = n - 1; j >= i; j--) {
+                if(isMonotonic(nums, i, j)) {
+                    // cout << '(' << i << ',' << j << ')' << endl;
+                    mx = max(mx, j - i + 1);
+                }
+            }
+        }
+        return mx;
+    }
+};
