1a1f29e25e2683b7b757cc6b4811b3d508621122
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-02-25 22:35:03 -0500

    Time: 0 ms (100%), Space: 45.2 MB (51.09%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int kadane(vector<int>& nums, bool mini) {
-        int n = nums.size(), res = nums[0], peak = nums[0];
-        if(mini) {
-            for(int i = 1; i < n; i++) {
-                peak = min(peak + nums[i], nums[i]);
-                res = min(peak, res);
-            } 
-        } else {
-            for(int i = 1; i < n; i++) {
-                peak = max(peak + nums[i], nums[i]);
-                res = max(peak, res);
-            }
-        }
-        return res;
-    }
     int maxAbsoluteSum(vector<int>& nums) {
-        int mx = 0, mn = 1e9 + 1;
-        mx = kadane(nums, false);
-        mn = kadane(nums, true);
-        return max(mx, -mn);
+        int minPrefixSum = 0, maxPrefixSum = 0;
+
+        int prefixSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            prefixSum += nums[i];
+
+            minPrefixSum = min(minPrefixSum, prefixSum);
+            maxPrefixSum = max(maxPrefixSum, prefixSum);
+        }
+
+        return maxPrefixSum - minPrefixSum;
     }
 };
