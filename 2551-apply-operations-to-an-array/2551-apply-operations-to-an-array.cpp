83f95e4ded671dcb799210acaf48fc876ed94277
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-01 11:44:10 -0500

    Time: 0 ms (100%), Space: 13 MB (87.59%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -9,11 +9,10 @@ public:
                 nums[i+1] = 0;
             }
         }
+        int last_zero = 0;
         for(int i = 0; i < n; i++) {
-            if(nums[i] != 0) res.push_back(nums[i]);
+            if(nums[i]) swap(nums[last_zero++], nums[i]);
         }
-        int k = res.size();
-        for(int i = k; i < n; i++) res.push_back(0);
-        return res;
+        return nums;
     }
 };
