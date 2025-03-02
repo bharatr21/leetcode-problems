81277942ee0e977c7e056aa75f721156fd5d18ae
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-01 21:34:51 -0500

    Time: 3 ms (32.71%), Space: 15.3 MB (17.09%) - LeetHub

---
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        unordered_map<int, int> store;
+        for(auto item: nums1) {
+            int id = item[0], score = item[1];
+            store[id] = score;
+        }
+        for(auto item: nums2) {
+            int id = item[0], score = item[1];
+            if(store.count(id)) store[id] += score;
+            else store[id] = score;
+        }
+        vector<vector<int>> res;
+        for(auto u: store) res.push_back({u.first, u.second});
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
