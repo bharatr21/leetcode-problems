77d53dceec57f1282b4f496e593facebbeae59e0
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-12 14:31:20 -0400

    Time: 0 ms (100%), Space: 8.9 MB (68.6%) - LeetHub

--- 1084-find-k-length-substrings-with-no-repeated-characters/1084-find-k-length-substrings-with-no-repeated-characters.cpp
+++ 1084-find-k-length-substrings-with-no-repeated-characters/1084-find-k-length-substrings-with-no-repeated-characters.cpp
@@ -3,15 +3,15 @@ public:
     int numKLenSubstrNoRepeats(string s, int k) {
         int n = s.size();
         if(k > n) return 0;
-        vector<int> hashs(26, 0);
+        vector<int> hash(26, 0);
         int rep = 0, res = 0;
         for(int i = 0; i < k; i++) {
-            if(++hashs[s[i] - 'a'] == 2) rep++;
+            if(++hash[s[i] - 'a'] == 2) rep++;
         }
         if(!rep) res++;
         for(int i = k; i < n; i++) {
-            if(--hashs[s[i-k] - 'a'] == 1) rep--;
-            if(++hashs[s[i] - 'a'] == 2) rep++;
+            if(--hash[s[i-k] - 'a'] == 1) rep--;
+            if(++hash[s[i] - 'a'] == 2) rep++;
             if(!rep) res++;
         }
         return res;
