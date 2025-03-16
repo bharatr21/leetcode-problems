3588f841c9d3a5b8cadfadc11755ba407d01e685
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-16 15:10:06 -0400

    Time: 0 ms (100%), Space: 13.8 MB (17.76%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i = 0, res = 0;
-        while (i < chars.size()) {
-            int groupLength = 1;
-            while (i + groupLength < chars.size() && chars[i + groupLength] == chars[i]) {
-                groupLength++;
-            }
+        int i = 0, res = 0, n = chars.size();
+        while(i < n) {
+            int rl = 1;
+            while(i + rl < n && chars[i + rl] == chars[i]) rl++;
             chars[res++] = chars[i];
-            if (groupLength > 1) {
-                for (char c : to_string(groupLength)) {
+            if(rl > 1) {
+                for(char c: to_string(rl)) {
                     chars[res++] = c;
                 }
             }
-            i += groupLength;
+            i += rl;
         }
         return res;
     }
