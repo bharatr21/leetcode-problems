009bcf29fd8c6626506cc705c12829596e776717
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-17 12:13:24 -0400

    Time: 0 ms (100%), Space: 8.5 MB (23.09%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     string smallestNumber(string pattern) {
-        return to_string(findSmallestNumber(pattern, 0, 0, 0));
-    }
+        string result;
+        stack<int> numStack;
 
-private:
-    // Recursively find the smallest number that satisfies the pattern
-    int findSmallestNumber(string pattern, int currentPosition,
-                           int usedDigitsMask, int currentNum) {
-        // Base case: return the current number when the whole pattern is
-        // processed
-        if (currentPosition > pattern.size()) return currentNum;
+        // Iterate through the pattern
+        for (int index = 0; index <= pattern.size(); index++) {
+            // Push the next number onto the stack
+            numStack.push(index + 1);
 
-        int result = INT_MAX;
-        int lastDigit = currentNum % 10;
-        bool shouldIncrement =
-            currentPosition == 0 || pattern[currentPosition - 1] == 'I';
-
-        // Try all possible digits (1 to 9) that are not yet used and follow the
-        // pattern
-        for (int currentDigit = 1; currentDigit <= 9; ++currentDigit) {
-            if ((usedDigitsMask & 1 << currentDigit) == 0 &&
-                currentDigit > lastDigit == shouldIncrement)
-                result = min(result, findSmallestNumber(
-                                         pattern, currentPosition + 1,
-                                         usedDigitsMask | 1 << currentDigit,
-                                         currentNum * 10 + currentDigit));
+            // If 'I' is encountered or we reach the end, pop all stack elements
+            if (index == pattern.size() || pattern[index] == 'I') {
+                while (!numStack.empty()) {
+                    result += to_string(numStack.top());
+                    numStack.pop();
+                }
+            }
         }
 
         return result;
