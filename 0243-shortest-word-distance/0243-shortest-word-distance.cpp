e01f362a4ad58fe8279b436d807eb33f7a2228e1
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-19 03:00:39 -0400

    Time: 2 ms (27.06%), Space: 15.6 MB (56.93%) - LeetHub

--- 0243-shortest-word-distance/0243-shortest-word-distance.cpp
+++ 0243-shortest-word-distance/0243-shortest-word-distance.cpp
@@ -2,34 +2,12 @@ class Solution {
 public:
     int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
         int n = wordsDict.size();
-        unordered_map<string, vector<int>> idxMap;
-        for(int i = 0; i < n; i++) {
-            string word = wordsDict[i];
-            idxMap[word].push_back(i);
-        }
-        int res = n + 1;
-        auto v1 = idxMap[word1], v2 = idxMap[word2]; 
-        int s1 = v1.size(), s2 = v2.size();
-        if(s1 < s2) {
-            for(int pos: v1) {
-                int lb = lower_bound(v2.begin(), v2.end(), pos) - v2.begin();
-                if(lb > 0) lb--;
-                int ub = upper_bound(v2.begin(), v2.end(), pos) - v2.begin();
-                cout << pos << ' ' << v2[lb] << ' ' << ((ub < s2) ? v2[ub]: '#');
-                res = min(res, abs(pos - v2[lb]));
-                if(ub < s2) res = min(res,  abs(v2[ub] - pos));
-            }
-            cout << endl;
-        } else {
-            for(int pos: v2) {
-                int lb = lower_bound(v1.begin(), v1.end(), pos) - v1.begin();
-                if(lb > 0) lb--;
-                int ub = upper_bound(v1.begin(), v1.end(), pos) - v1.begin();
-                cout << pos << ' ' << v1[lb] << ' ' << ((ub < s1) ? v1[ub] : '#');
-                res = min(res, abs(pos - v1[lb]));
-                if(ub < s1) res = min(res, abs(v1[ub] - pos));
-            }
-            cout << endl;
+        int idx1 = -n-1, idx2 = -2*n-1, i = 0, res = n + 2;
+        for(string word: wordsDict) {
+            if(word == word1) idx1 = i;
+            else if(word == word2) idx2 = i;
+            i++;
+            res = min(res, abs(idx1 - idx2));
         }
         return res;
     }
