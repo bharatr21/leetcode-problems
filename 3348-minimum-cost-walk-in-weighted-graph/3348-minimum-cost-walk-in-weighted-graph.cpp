8f5a49d1eeb81007a7d565892851b516d688fdba
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-20 18:03:27 -0400

    Time: 14 ms (94.55%), Space: 204.3 MB (93.18%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,74 +1,56 @@
+class DSU {
+    public:
+        int size;
+        vector<int> parent;
+        vector<int> rank;
+    DSU(int n): size(n) {
+        parent.resize(n);
+        rank.resize(n);
+        for(int i = 0; i < n; i++) parent[i] = i;
+        for(int i = 0; i < n; i++) rank[i] = 1;
+    }
+
+    int findParent(int x) {
+        if(parent[x] == x) return x;
+        return (parent[x] = findParent(parent[x]));
+    }
+
+    void Union(int x, int y) {
+        x = findParent(x);
+        y = findParent(y);
+        if(x == y) return;
+        if(rank[x] < rank[y]) {
+            parent[x] = y;
+        } else {
+            parent[y] = x;
+            if(rank[x] == rank[y]) rank[x]++;
+        }
+    }
+};
+
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> depth;
-
-    vector<int> minimumCost(int n, vector<vector<int>>& edges,
-                            vector<vector<int>>& queries) {
-        // Initialize the parent array with -1 as initially each node belongs to
-        // its own component
-        parent.resize(n, -1);
-        depth.resize(n, 0);
-
-        // All values are initially set to the number with only 1s in its binary
-        // representation
-        vector<unsigned int> componentCost(n, -1);
-
-        // Construct the connected components of the graph
-        for (auto& edge : edges) {
-            Union(edge[0], edge[1]);
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
+        auto dsu = DSU(n);
+        for(auto& edge: edges) {
+            dsu.Union(edge[0], edge[1]);
         }
-
-        // Calculate the cost of each component by performing bitwise AND of all
-        // edge weights in it
-        for (auto& edge : edges) {
-            int root = find(edge[0]);
-            componentCost[root] &= edge[2];
+        vector<unsigned int> cost(n, -1);
+        for(auto& edge: edges) {
+            int par = dsu.findParent(edge[0]);
+            cost[par] = cost[par] & edge[2];
         }
-
-        vector<int> answer;
-        for (auto& query : queries) {
-            int start = query[0];
-            int end = query[1];
-
-            // If the two nodes are in different connected components, return -1
-            if (find(start) != find(end)) {
-                answer.push_back(-1);
-            } else {
-                // Find the root of the edge's component
-                int root = find(start);
-                // Return the precomputed cost of the component
-                answer.push_back(componentCost[root]);
+        vector<int> res;
+        for(auto& query: queries) {
+            int start = query[0], end = query[1];
+            if(dsu.findParent(start) != dsu.findParent(end)) {
+                res.push_back(-1);
+            }
+            else {
+                int par = dsu.findParent(start);
+                res.push_back(cost[par]);
             }
         }
-        return answer;
-    }
-
-private:
-    // Find function to return the root (representative) of a node's component
-    int find(int node) {
-        // If the node is its own parent, it is the root of the component
-        if (parent[node] == -1) return node;
-        // Otherwise, recursively find the root and apply path compression
-        return parent[node] = find(parent[node]);
-    }
-
-    // Union function to merge the components of two nodes
-    void Union(int node1, int node2) {
-        int root1 = find(node1);
-        int root2 = find(node2);
-
-        // If the two nodes are already in the same component, do nothing
-        if (root1 == root2) return;
-
-        // Union by depth: ensure the root of the deeper tree becomes the parent
-        if (depth[root1] < depth[root2]) swap(root1, root2);
-
-        // Merge the two components by making root1 the parent of root2
-        parent[root2] = root1;
-
-        // If both components had the same depth, increase the depth of the new
-        // root
-        if (depth[root1] == depth[root2]) depth[root1]++;
+        return res;
     }
 };
