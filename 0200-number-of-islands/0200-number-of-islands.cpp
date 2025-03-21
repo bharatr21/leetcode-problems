50b14d86bb2baa2a7fcd71c1ac3079aadbacc809
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-21 16:31:42 -0400

    Time: 45 ms (15%), Space: 22.7 MB (36.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,38 +1,28 @@
 class Solution {
-public:
-    bool isValid(vector<vector<char>>& grid, int i, int j)
-    {
-        int m = grid.size(), n = grid[0].size();
-        return (i >= 0 && i < m) && (j >= 0 && j < n);
+public: 
+    bool isValid(int x, int y, int m, int n) {
+        return (x >= 0 && x < m && y >= 0 && y < n);
     }
-    void dfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int r, int c)
-    {
-        vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int i;
-        for(i = 0; i < 4; i++)
-        {
-            int dx = dirs[i][0], dy = dirs[i][1];
-            int nr = r + dx, nc = c + dy;
-            if(isValid(grid, nr, nc) && grid[nr][nc] == '1' && !vis[nr][nc])
-            {
-                vis[nr][nc] = 1;
-                dfs(grid, vis, nr, nc);
+    void dfs(int x, int y, int m, int n, vector<vector<bool>>& vis, vector<vector<char>>& grid) {
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vis[x][y] = true;
+        for(int i = 0; i < 4; i++) {
+            auto& [dx, dy] = dirs[i];
+            int nx = x + dx, ny = y + dy;
+            if(isValid(nx, ny, m, n) && !vis[nx][ny] && grid[nx][ny] == '1') {
+                dfs(nx, ny, m, n, vis, grid);
             }
         }
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size(), i, j;
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        int ct = 0;
-        for(i = 0; i < m; i++)
-        {
-            for(j = 0; j < n; j++)
-            {
-                if(grid[i][j] == '1' && !vis[i][j])
-                {
-                    vis[i][j] = 1;
+        int m = grid.size(), n = grid[0].size(), ct = 0;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!vis[i][j] && grid[i][j] == '1') {
                     ct++;
-                    dfs(grid, vis, i, j);
+                    dfs(i, j, m, n, vis, grid);
                 }
             }
         }
