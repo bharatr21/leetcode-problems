39200db17f98b816ce2de42f8f5830c8b59d4606
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-27 01:37:30 -0400

    Time: 0 ms (100%), Space: 12.9 MB (82.74%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.cpp
+++ 0360-sort-transformed-array/0360-sort-transformed-array.cpp
@@ -1,12 +1,39 @@
 class Solution {
 public:
+    int transform(int& x, int& a, int& b, int& c) {
+        return (a * x * x + b * x + c);
+    }
+
     vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
         int n = nums.size();
-        vector<int> qnums(n);
-        for(int i = 0; i < n; i++) {
-            qnums[i] = (a * nums[i] * nums[i] + b * nums[i] + c);
+        int left = 0, right = n - 1;
+        vector<int> res;
+        if(a < 0) {
+            while(left <= right) {
+                int lt = transform(nums[left], a, b, c);
+                int rt = transform(nums[right], a, b, c);
+                if(lt < rt) {
+                    res.push_back(lt);
+                    left++;
+                } else {
+                    res.push_back(rt);
+                    right--;
+                }
+            }
+        } else {
+            while(left <= right) {
+                int lt = transform(nums[left], a, b, c);
+                int rt = transform(nums[right], a, b, c);
+                if(lt > rt) {
+                    res.push_back(lt);
+                    left++;
+                } else {
+                    res.push_back(rt);
+                    right--;
+                }
+            }
+            reverse(res.begin(), res.end());
         }
-        sort(qnums.begin(), qnums.end());
-        return qnums;
+        return res;
     }
 };
