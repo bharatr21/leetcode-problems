9810985377091269d42cf76e951ee524cca2499a
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-28 14:30:33 -0400

    Time: 0 ms (100%), Space: 14.5 MB (84.97%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.cpp
+++ 0265-paint-house-ii/0265-paint-house-ii.cpp
@@ -2,22 +2,38 @@ class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
         int n = costs.size(), k = costs[0].size();
-        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+        int prevMin = -1, prevSecMin = -1, prevMinIdx = -1;
         for(int i = 0; i < k; i++) {
-            dp[0][i] = costs[0][i];
-        }
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < k; j++) {
-                dp[i][j] = costs[i][j];
-                int mn = 2001;
-                for(int p = 0; p < k; p++) {
-                    if(p != j) mn = min(mn, dp[i-1][p]);
-                }
-                dp[i][j] += mn;
+            int cur = costs[0][i];
+            if(prevMin == -1 || cur < prevMin) {
+                prevSecMin = prevMin;
+                prevMinIdx = i;
+                prevMin = cur;
+            } else if(prevSecMin == -1 || cur < prevSecMin) {
+                prevSecMin = cur;
             }
         }
-        int res = 2001;
-        for(int i = 0; i < k; i++) res = min(res, dp[n-1][i]);
-        return res;
+        for(int i = 1; i < n; i++) {
+            int mn = -1, secmn = -1, minIdx = -1;
+            for(int j = 0; j < k; j++) {
+                int cur = costs[i][j];
+                if(j == prevMinIdx) {
+                    cur += prevSecMin;
+                } else {
+                    cur += prevMin;
+                }
+                if(mn == -1 || cur < mn) {
+                    secmn = mn;
+                    minIdx = j;
+                    mn = cur;
+                } else if(secmn == -1 || cur < secmn) {
+                    secmn = cur;
+                }
+            }
+            prevMin = mn;
+            prevSecMin = secmn;
+            prevMinIdx = minIdx;
+        }
+        return prevMin;
     }
 };
