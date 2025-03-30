3f824d19c2f5fdcbd2114e121b73ff88dea466c7
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-29 20:47:19 -0400

    Time: 0 ms (100%), Space: 8.9 MB (71.56%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char, pair<int, int>> mp;
-        vector<pair<int, int>> vp;
-        int i = 0;
-        for(char ch: s) {
-            if(!mp.count(ch)) {
-                mp[ch] = {i, i};
-            }
-            else {
-                mp[ch].second = i;
-            }
-            i++;
+        // Stores the last index of each character in 's'
+        vector<int> lastOccurrence(26, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            lastOccurrence[s[i] - 'a'] = i;
         }
-        for(auto& interval: mp) {
-            vp.push_back(interval.second);
-        }
-        sort(vp.begin(), vp.end());
-        // for(auto& el: vp) cout << "[" << el.first << "," << el.second << "]" << '\n';
-        int n = vp.size();
-        vector<pair<int, int>> pres;
-        vector<int> res;
-        pair<int, int> cur_interval = vp[0];
-        for(int i = 1; i < n; i++) {
-            auto [st, en] = vp[i];
-            auto [cur_st, cur_en] = cur_interval;
-            if(st > cur_en) {
-                pres.push_back(cur_interval);
-                cur_interval = vp[i];
-            } else if(en < cur_st) {
-                pres.push_back(vp[i]);   
-            } else {
-                cur_interval = {min(st, cur_st), max(en, cur_en)};
+
+        int partitionEnd = 0, partitionStart = 0;
+        vector<int> partitionSizes;
+        for (int i = 0; i < s.size(); ++i) {
+            partitionEnd = max(partitionEnd, lastOccurrence[s[i] - 'a']);
+            // End of the current partition
+            if (i == partitionEnd) {
+                partitionSizes.push_back(i - partitionStart + 1);
+                partitionStart = i + 1;
             }
         }
-        pres.push_back(cur_interval);
-        // cout << "\nMerged intervals: " << endl;
-        // for(auto& el: pres) cout << "[" << el.first << "," << el.second << "]" << '\n';
-        for(auto& pel: pres) {
-            res.push_back(pel.second - pel.first + 1);
-        }
-        return res;
+        return partitionSizes;
     }
 };
