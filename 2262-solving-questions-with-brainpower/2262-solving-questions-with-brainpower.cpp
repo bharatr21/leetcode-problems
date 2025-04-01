3ff226537089c0df0642a4c2bf2ccddd22c91119
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-03-31 20:50:21 -0400

    Time: 76 ms (5.54%), Space: 143.7 MB (7.13%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    long solve(int idx, vector<pair<int, int>>& qinfo, vector<long>& dp) {
-        int n = qinfo.size();
-        if(idx >= n) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        long pick = qinfo[idx].first + solve(idx + qinfo[idx].second + 1, qinfo, dp);
-        long notPick = solve(idx + 1, qinfo, dp);
-        return (dp[idx] = max(pick, notPick));
-    }
-
     long long mostPoints(vector<vector<int>>& questions) {
         vector<pair<int, int>> qinfo;
-        for(vector<int> q: questions) {
-            qinfo.push_back({q[0], q[1]});
-        }
+        for(vector<int> q: questions) qinfo.push_back({q[0], q[1]});
         int n = qinfo.size();
-        vector<long> dp(n+1, -1);
+        vector<long> dp(n, -1);
         dp[n-1] = qinfo[n-1].first;
-        solve(0, qinfo, dp);
+        for(int i = n - 2; i >= 0; i--) {
+            dp[i] = dp[i+1];
+            if(i + qinfo[i].second + 1 < n) {
+                dp[i] = max(dp[i], qinfo[i].first + dp[i + qinfo[i].second + 1]);
+            } else {
+                dp[i] = max(dp[i], 1L * qinfo[i].first);
+            }
+        }
         return dp[0];
     }
 };
