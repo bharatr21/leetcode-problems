ad693c1e791f187554486ee5d46b756de8df6e33
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-07 11:30:01 -0400

    Time: 207 ms (11.01%), Space: 184.7 MB (10.42%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
+    bool subset(int idx, int tgt, vector<int>& nums, vector<vector<int>>& dp) {
         int n = nums.size();
+        if(tgt == 0) return true;
+        else if(idx == n || tgt < 0) return false;
+        else if(dp[idx][tgt] != -1) return dp[idx][tgt];
+        bool pick = subset(idx + 1, tgt - nums[idx], nums, dp);
+        bool notPick = subset(idx + 1, tgt, nums, dp);
+        return dp[idx][tgt] = (pick || notPick);
+    }
+
+    bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum % 2) return false;
-        int target = sum / 2;
-        vector<int> cur(target+1, 0), next(target+1, 0);
-        for(int i = 0; i <= n; i++) cur[0] = 1;
-        for(int i = 1; i <= target; i++) next[i] = 0;
-        for(int idx = n - 1; idx >= 0; idx--) {
-            for(int j = 1; j <= target; j++) {
-                if(nums[idx] <= j) {
-                    cur[j] = (next[j - nums[idx]] || next[j]);
-                }
-                else {
-                    cur[j] = next[j];
-                }
-            }
-            next = cur;
-        }        
-        return cur[target];
+        int tgt = sum / 2, n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
+        return subset(0, tgt, nums, dp);
     }
 };
