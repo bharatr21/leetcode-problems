5d16884f429ff4d5093ea60aef8cb2fecbae37ab
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-07 20:43:44 -0400

    Time: 103 ms (68.71%), Space: 13.4 MB (86.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -4,17 +4,17 @@ public:
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum % 2) return false;
         int tgt = sum / 2, n = nums.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(tgt + 1, false));
-        for(int i = 0; i < n; i++) dp[i][0] = true;
-        for(int i = 1; i <= tgt; i++) dp[n-1][i] = false;
+        vector<bool> prev(tgt + 1, false), cur(tgt + 1, false);
         for(int i = n - 2; i >= 0; i--)
         {
+            prev[0] = true;
             for(int j = 1; j <= tgt; j++) 
             {
-                dp[i][j] = dp[i][j] | dp[i+1][j];
-                if(nums[i+1] <= j) dp[i][j] = dp[i][j] | dp[i+1][j - nums[i+1]];
+                cur[j] = cur[j] | prev[j];
+                if(nums[i+1] <= j) cur[j] = cur[j] | prev[j - nums[i+1]];
             }
+            prev = cur;
         }
-        return dp[0][tgt];
+        return prev[tgt];
     }
 };
