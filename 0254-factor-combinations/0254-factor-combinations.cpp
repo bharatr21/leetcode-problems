ab92cf5859c1b247a3e2e6b74e0d82f3f021a998
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-14 18:19:45 -0400

    Time Limit Exceeded (but seems correct) - 0254-factor-combinations.cpp

--- 0254-factor-combinations/0254-factor-combinations.cpp
+++ 0254-factor-combinations/0254-factor-combinations.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    void backtrack(vector<int>& factors, vector<vector<int>>& res) {
-        if(factors.size() > 1) res.push_back(factors);
-        int last = factors.back();
-        factors.pop_back();
-        for(int i = factors.empty() ? 2 : factors.back(); i <= last / i; i++) {
-            if(last % i == 0) {
-                factors.push_back(i);
-                factors.push_back(last / i);
-                backtrack(factors, res);
-                factors.pop_back();
-                factors.pop_back();
+    void solve(int idx, vector<int>& factors, vector<int>& tmp, int rem, set<vector<int>>& res) {
+        int n = factors.size();
+        if(idx >= n) return;
+        if(rem == 1) {res.insert(tmp); return;}
+        for(int i = idx; i < n; i++) {
+            if(rem % factors[i] == 0) {
+                tmp.push_back(factors[i]);
+                rem = rem / factors[i];
+                solve(i, factors, tmp, rem, res);
+                tmp.pop_back();
+                rem = rem * factors[i];
+                solve(i + 1, factors, tmp, rem, res);
             }
         }
-        factors.push_back(last);
     }
-
+    
     vector<vector<int>> getFactors(int n) {
-        vector<int> factors = {n};
-        vector<vector<int>> res;
-        backtrack(factors, res);
-        return res;
+        set<vector<int>> res;
+        vector<int> factors, tmp;
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                factors.push_back(i);
+                factors.push_back(n / i);
+            }
+        }
+        sort(factors.begin(), factors.end());
+        solve(0, factors, tmp, n, res);
+        vector<vector<int>> ans(res.begin(), res.end());
+        return ans;
     }
 };
