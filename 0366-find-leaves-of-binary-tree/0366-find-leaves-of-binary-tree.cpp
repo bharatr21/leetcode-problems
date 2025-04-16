244ec63565b3c42f255e5b31148783489c279d7f
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-16 15:11:41 -0400

    Time: 0 ms (100%), Space: 11.4 MB (71.51%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.cpp
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.cpp
@@ -11,25 +11,20 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if(!root) return 0;
-        return 1 + max(height(root->left), height(root->right));
-    }
-
-    void helper(vector<vector<int>>& heights, TreeNode* node) {
-        int curHeight = height(node);
-        heights[curHeight - 1].push_back(node->val);
-        if(node->left) helper(heights, node->left);
-        if(node->right) helper(heights, node->right);
+    int heightHelper(vector<vector<int>>& heights, TreeNode* node) {
+        if(!node) return -1;
+        int leftHeight = heightHelper(heights, node->left);
+        int rightHeight = heightHelper(heights, node->right);
+        int curHeight = 1 + max(leftHeight, rightHeight);
+        if(heights.size() == curHeight) heights.resize(curHeight + 1);
+        heights[curHeight].push_back(node->val);
+        return curHeight;
     }
 
     vector<vector<int>> findLeaves(TreeNode* root) {
         vector<vector<int>> heights;
         if(!root) return heights;
-        int totHeight = height(root);
-        heights.resize(totHeight + 1);
-        helper(heights, root);
-        heights.pop_back();
+        heightHelper(heights, root);
         return heights;
     }
 };
