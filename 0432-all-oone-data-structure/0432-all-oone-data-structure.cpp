2ae113139d29e7ddafac89c76e95031fe5bd4b7d
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-17 18:23:07 -0400

    Time: 79 ms (61.02%), Space: 98.2 MB (15.23%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,75 +1,58 @@
 class AllOne {
+public:
     struct Node {
         int freq;
         unordered_set<string> keys;
     };
     list<Node> freqList;
     unordered_map<string, list<Node>::iterator> key2it;
-public:
     AllOne() {
         
     }
     
     void inc(string key) {
-        // Find current node (or create a frequency-0 dummy)
-        auto it = key2it.count(key)
-                  ? key2it[key]
-                  : freqList.insert(freqList.begin(), Node{0, {}});
-        int f = it->freq;
-        auto next = std::next(it);
-
-        // Ensure the next node has freq = f+1
-        if (next == freqList.end() || next->freq != f + 1) {
-            next = freqList.insert(next, Node{f + 1, {}});
+        auto nodeIt = key2it.count(key) ? key2it[key] : 
+        freqList.insert(freqList.begin(), Node{0, {}});
+        int f = nodeIt->freq;
+        auto nextNode = next(nodeIt);
+        if(nextNode == freqList.end() || nextNode->freq != f + 1) {
+            nextNode = freqList.insert(nextNode, Node{f + 1, {}});
         }
+        nextNode->keys.insert(key);
+        key2it[key] = nextNode;
 
-        // Move key to next node
-        next->keys.insert(key);
-        key2it[key] = next;
-        
-        // Remove from old node; if empty, erase it
-        if (it->keys.erase(key), it->keys.empty()) {
-            freqList.erase(it);
-        }
+        nodeIt->keys.erase(key);
+        if(nodeIt->keys.empty()) freqList.erase(nodeIt);
     }
     
     void dec(string key) {
         auto mapIt = key2it.find(key);
-        if (mapIt == key2it.end()) return;  // Key not present
-
+        if(mapIt == key2it.end()) return;
         auto nodeIt = mapIt->second;
         int f = nodeIt->freq;
-        nodeIt->keys.erase(key);
+        if(f > 1) {
+            auto pre = (nodeIt == freqList.begin()) ? freqList.end(): prev(nodeIt);
 
-        if (f > 1) {
-            auto prev = (nodeIt == freqList.begin())
-                        ? freqList.end()
-                        : std::prev(nodeIt);
-            // Ensure the previous node has freq = f-1
-            if (prev == freqList.end() || prev->freq != f - 1) {
-                prev = freqList.insert(nodeIt, Node{f - 1, {}});
+            if(pre == freqList.end() || pre->freq != f - 1) {
+                pre = freqList.insert(nodeIt, Node{f - 1, {}});
             }
-            prev->keys.insert(key);
-            key2it[key] = prev;
+            pre->keys.insert(key);
+            key2it[key] = pre;
         } else {
-            // Frequency hits zero: remove key entirely
             key2it.erase(mapIt);
         }
-
-        // If old node is empty now, erase it
-        if (nodeIt->keys.empty()) {
-            freqList.erase(nodeIt);
-        }
+        nodeIt->keys.erase(key);
+        if(nodeIt->keys.empty()) freqList.erase(nodeIt);
     }
     
     string getMaxKey() {
         if (freqList.empty()) return "";
-        return *freqList.back().keys.begin();
+        return *(freqList.back().keys.begin());
     }
     
     string getMinKey() {
         if (freqList.empty()) return "";
-        return *freqList.front().keys.begin();
+        return *(freqList.front().keys.begin());
     }
 };
 
