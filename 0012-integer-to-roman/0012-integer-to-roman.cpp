6f17b823904c63af0c3bf90eaddc0f054c3df0ae
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-22 11:30:21 -0400

    Time: 5 ms (37.1%), Space: 15.4 MB (6.88%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        vector<int> values = {1000, 900, 500, 400, 100, 90, 50,
-                              40,   10,  9,   5,   4,   1};
-        vector<string> symbols = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
-                                  "XL", "X",  "IX", "V",  "IV", "I"};
-        string roman;
-        // Loop through each symbol, stopping if num becomes 0.
-        for (int i = 0; i < values.size() && num > 0; i++) {
-            // Repeat while the current symbol still fits into num.
-            while (values[i] <= num) {
-                num -= values[i];
-                roman += symbols[i];
-            }
-        }
-        return roman;
+    std::string intToRoman(int num) {
+        std::vector<std::string> thousands = {"", "M", "MM", "MMM"};
+        std::vector<std::string> hundreds = {"",  "C",  "CC",  "CCC",  "CD",
+                                             "D", "DC", "DCC", "DCCC", "CM"};
+        std::vector<std::string> tens = {"",  "X",  "XX",  "XXX",  "XL",
+                                         "L", "LX", "LXX", "LXXX", "XC"};
+        std::vector<std::string> ones = {"",  "I",  "II",  "III",  "IV",
+                                         "V", "VI", "VII", "VIII", "IX"};
+        return thousands[num / 1000] + hundreds[num % 1000 / 100] +
+               tens[num % 100 / 10] + ones[num % 10];
     }
 };
