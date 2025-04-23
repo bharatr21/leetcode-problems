b13b58384f50742c8eaadda81b51b83e3dfa7ec9
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-23 15:34:41 -0400

    Time: 0 ms (100%), Space: 8.2 MB (86.51%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        int n = s.size();
-        string res;
         vector<int> fre(26, 0);
-        priority_queue<vector<int>> pq;
         for(char ch: s) fre[ch - 'a']++;
+        int mx = -1, letter = 0;
         for(int i = 0; i < 26; i++) {
-            if(fre[i] > (n + 1)/2) return "";
-            if(fre[i] > 0) pq.push(vector<int>{fre[i], i + 'a'});
+            if(mx < fre[i]) {
+                mx = fre[i];
+                letter = i;
+            }
         }
-        while(!pq.empty()) {
-            auto first = pq.top();
-            pq.pop();
-            if(res.empty() || first[1] != res.back()) {
-                res += (char)first[1];
-                if(--first[0] > 0) pq.push(first);
-            } else {
-                if(pq.empty()) return "";
-                auto sec = pq.top();
-                pq.pop();
-                res += (char)sec[1];
-                if(--sec[0] > 0) pq.push(sec);
-                pq.push(first);
+        if(fre[letter] > (s.size() + 1) / 2) return "";
+        string res = s;
+        int idx = 0;
+        while(fre[letter]) {
+            res[idx] = char(letter + 'a');
+            idx += 2;
+            fre[letter]--;
+        }
+        for(int i = 0; i < 26; i++) {
+            while(fre[i]) {
+                if(idx >= s.size()) idx = 1;
+                res[idx] = char(i + 'a');
+                idx += 2;
+                fre[i]--;
             }
         }
         return res;
