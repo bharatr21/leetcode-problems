42d2a71e4ebf14b926549a6222730ef4f5a8d4e6
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-25 19:06:02 -0400

    Time: 36 ms (43.06%), Space: 23.4 MB (47.62%) - LeetHub

--- 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.cpp
+++ 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
     vector<string> mostVisitedPattern(vector<string>& username, vector<int>& timestamp, vector<string>& website) {
-        set<tuple<int, string, string>> vis;
-        map<tuple<string, string, string>, int> pattern;
-        set<tuple<string, string, string, string>> store;
+        int n = username.size(), mx = -1;
+        vector<string> res;
+        vector<tuple<int, string, string>> logs;
         unordered_map<string, vector<string>> hist;
-        int n = username.size();
+        map<vector<string>, int> hits;
+        set<tuple<string, string, string>> seen;
         for(int i = 0; i < n; i++) {
-            vis.insert({timestamp[i], username[i], website[i]});
+            logs.push_back({timestamp[i], username[i], website[i]});
         }
-        for(auto& el: vis) {
-            auto [timestamp, user, website] = el;
-            hist[user].push_back(website);
+        sort(logs.begin(), logs.end());
+        for(auto& item: logs) {
+            auto [timestamp, user, site] = item;
+            hist[user].push_back(site);
         }
-        for(auto& h: hist) {
-            auto [user, websites] = h;
-            int n = websites.size();
-            for(int i = 0; i < n; i++) {
-                for(int j = i + 1; j < n; j++) {
-                    for(int k = j + 1; k < n; k++) {
-                        if(!store.count({user, websites[i], websites[j], websites[k]})) {
-                            pattern[{websites[i], websites[j], websites[k]}]++;
-                            store.insert({user, websites[i], websites[j], websites[k]});
+        for(auto& item: hist) {
+            string user = item.first;
+            vector<string> sites = item.second;
+            int m = hist[user].size();
+            seen.clear();
+            if(m < 3) continue;
+            for(int i = 0; i < m; i++) {
+                for(int j = i + 1; j < m; j++) {
+                    for(int k = j + 1; k < m; k++) {
+                        string site1 = sites[i], site2 = sites[j], site3 = sites[k];
+                        if(!seen.count({site1, site2, site3})) {
+                            hits[{site1, site2, site3}]++;
+                            seen.insert({site1, site2, site3});
                         }
                     }
                 }
             }
         }
-        int mx = -1;
-        tuple<string, string, string> tmp;
-        for(auto& pat: pattern) {
-            if(pat.second > mx) {
-                mx = pat.second;
-                tmp = pat.first;
+        for(auto& item: hits) {
+            if(mx < item.second) {
+                mx = item.second;
+                res = item.first;
             }
         }
-        auto [s1, s2, s3] = tmp;
-        vector<string> res = {s1, s2, s3};
         return res;
     }
 };
