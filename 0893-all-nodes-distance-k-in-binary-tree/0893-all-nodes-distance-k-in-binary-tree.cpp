5017a805a8949259d4bd945a9932949d56387f5b
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-27 14:32:52 -0400

    Time: 10 ms (10.84%), Space: 15.8 MB (25.11%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void traverse(TreeNode* root, unordered_map<int, vector<int>>& adj) {
+        if(!root) return;
+        if(root->left) {
+            adj[root->left->val].push_back(root->val);
+            adj[root->val].push_back(root->left->val);
+            traverse(root->left, adj);
+        }
+        if(root->right) {
+            adj[root->right->val].push_back(root->val);
+            adj[root->val].push_back(root->right->val);
+            traverse(root->right, adj);
+        }
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<int, vector<int>> adj;
+        vector<int> res;
+        if(!root || !target) return res;
+        traverse(root, adj);
+        queue<pair<int, int>> q;
+        unordered_set<int> seen;
+        q.push({target->val, 0});
+        while(!q.empty()) {
+            auto [val, depth] = q.front();
+            q.pop();
+            if(depth > k) continue;
+            if(depth == k) res.push_back(val);
+            seen.insert(val);
+            for(auto& neigh: adj[val]) {
+                if(!seen.count(neigh)) q.push({neigh, depth+1});
+            }
+        }
+        return res;
+    }
+};
