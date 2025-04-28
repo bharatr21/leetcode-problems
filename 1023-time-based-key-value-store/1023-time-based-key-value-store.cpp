6da1a0271c1e379611651803df002ab8d904147d
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-27 20:04:55 -0400

    Time: 38 ms (88.57%), Space: 136.2 MB (98.32%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
@@ -1,13 +1,13 @@
 class TimeMap {
 public:
-    unordered_map<string, vector<pair<int, string>>> store;
+    unordered_map<string, vector<string>> store;
     unordered_map<string, vector<int>> timer;
     TimeMap() {
 
     }
     
     void set(string key, string value, int timestamp) {
-        store[key].push_back({timestamp, value});
+        store[key].push_back(value);
         timer[key].push_back(timestamp);
     }
     
@@ -16,7 +16,7 @@ public:
         auto ub = upper_bound(timer[key].begin(), timer[key].end(), timestamp);
         if(ub == timer[key].begin() && *ub > timestamp || *prev(ub) > timestamp) return "";
         int idx = ((ub == timer[key].begin()) ? 0 : prev(ub) - timer[key].begin());
-        return store[key][idx].second;
+        return store[key][idx];
     }
 };
 
