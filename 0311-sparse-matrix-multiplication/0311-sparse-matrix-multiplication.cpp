47f5feae13cb02304eb5a8c94a881eeb78444fbb
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-04-29 01:35:43 -0400

    Time: 0 ms (100%), Space: 12.2 MB (70.85%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
@@ -3,10 +3,23 @@ public:
     vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
         int m = mat1.size(), p = mat1[0].size(), n = mat2[0].size();
         vector<vector<int>> res(m, vector<int>(n, 0));
+        vector<pair<int, int>> nz1, nz2;
         for(int i = 0; i < m; i++) {
+            for(int j = 0; j < p; j++) {
+                if(mat1[i][j]) nz1.push_back({i, j});
+            }
+        }
+        for(int i = 0; i < p; i++) {
             for(int j = 0; j < n; j++) {
-                for(int k = 0; k < p; k++) {
-                    res[i][j] += mat1[i][k] * mat2[k][j];
+                if(mat2[i][j]) nz2.push_back({i, j});
+            }
+        }
+        for(pair<int, int>& p1: nz1) {
+            for(pair<int, int>& p2: nz2) {
+                auto& [i1, j1] = p1;
+                auto& [i2, j2] = p2;
+                if(j1 == i2) {
+                    res[i1][j2] += mat1[i1][j1] * mat2[i2][j2];
                 }
             }
         }
