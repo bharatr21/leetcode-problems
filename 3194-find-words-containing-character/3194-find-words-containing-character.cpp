cd383e4027067ffd51d91d8e659acd68c1240f11
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-05-23 20:18:22 -0400

    Time: 6 ms (5.91%), Space: 34.7 MB (12.1%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.cpp
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -5,13 +5,7 @@ public:
         int n = words.size();
         vector<unordered_set<char>> charsets(n);
         for(int i = 0; i < n; i++) {
-            for(char ch: words[i]) {
-                charsets[i].insert(ch);
-            }
-        }
-        
-        for(int idx = 0; idx < n; idx++) {
-            if(charsets[idx].count(x)) res.push_back(idx);
+            if(words[i].find(x) != string::npos) res.push_back(i);
         }
         return res;
     }
