7c0b9f3d0f575fbdda6517abf3d82032ddbcc3c6
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-06-08 12:23:04 -0400

    Time: 0 ms (100%), Space: 19.5 MB (44.8%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
-    bool isValid(int i, int j, int m, int n)
-    {
-        return (i >= 0 && i < m) && (j >= 0 && j < n);
-    }
-    int INF = 2147483647;
-    void bfs(queue<pair<int, int>> &q, vector<vector<int>>& rooms)
-    {
-        int m = rooms.size(), n = rooms[0].size();
-        while(!q.empty())
-        {
-            vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-            auto [x, y] = q.front();
-            q.pop();
-            for(int k = 0; k < 4; k++)
-            {
-                int nx = x + dirs[k][0];
-                int ny = y + dirs[k][1];
-                while(isValid(nx, ny, m, n) && rooms[nx][ny] == INF)
-                {
-                    rooms[nx][ny] = rooms[x][y] + 1;
-                    q.push({nx, ny});
-                }
-            }
-        }
-    }
+    bool isValid(int x, int y, int m, int n) { return (x >= 0 && x < m && y >=0 && y < n);}
     void wallsAndGates(vector<vector<int>>& rooms) {
-        int m = rooms.size(), n = rooms[0].size(), i, j;
-        queue<pair<int, int>> q;
-        for(i = 0; i < m; i++)
-        {
-            for(j = 0; j < n; j++)
-            {
-                if(rooms[i][j] == 0)
-                {
-                    q.push({i, j});
+        int INF = 2147483647;
+        int m = rooms.size(), n = rooms[0].size();
+        queue<tuple<int, int, int>> q;
+        vector<pair<int, int>> dirs = {{0, 1}, {0 ,-1}, {1, 0}, {-1, 0}};
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(rooms[i][j] == 0) {
+                    q.push({0, i, j});
                 }
             }
         }
-        bfs(q, rooms);
+        while(!q.empty()) {
+            auto [dist, r, c] = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++) {
+                auto [dr, dc] = dirs[i];
+                int nr = r + dr, nc = c + dc;
+                if(isValid(nr, nc, m, n) && rooms[nr][nc] == INF) {
+                    rooms[nr][nc] = dist + 1;
+                    q.push({dist + 1, nr, nc});
+                }
+            }
+        }
     }
 };
