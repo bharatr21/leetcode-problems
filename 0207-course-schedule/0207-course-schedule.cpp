1cecdee318046b48d7de08e565defe05b98996f8
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-06-08 23:34:56 -0400

    Time: 0 ms (100%), Space: 20 MB (42.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    void topo(int v, vector<int>& indeg, vector<vector<int>>& adj, vector<int>& res, vector<int>& vis)
-    {
-        for(int u: adj[v])
-        {
-            indeg[u]--;
-            if(indeg[u] == 0 && !vis[u])
-            {
-                vis[u] = 1;
-                res.push_back(u);
-                topo(u, indeg, adj, res, vis);
-            }
-        }
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses, i;
-        vector<int> res;
+        int n = numCourses;
         vector<int> indeg(n, 0);
-        vector<int> vis(n, 0);
-        vector<vector<int>> adj(n, vector<int>());
-        for(vector<int> v: prerequisites)
-        {
-            int a = v[0], b = v[1];
-            indeg[a]++;
+        vector<vector<int>> adj(n);
+        for(vector<int>& e: prerequisites) {
+            int a = e[0], b = e[1];
             adj[b].push_back(a);
+            indeg[a]++;
         }
-        for(i = 0; i < n; i++)
-        {
-            if(indeg[i] == 0 && !vis[i])
-            {
-                vis[i] = 1;
-                res.push_back(i);
-                topo(i, indeg, adj, res, vis);
+        unordered_set<int> vis;
+        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            if(indeg[i] == 0) {
+                q.push(i);
             }
         }
-        return (res.size() == n);
+
+        while(!q.empty()) {
+            auto idx = q.front();
+            q.pop();
+            vis.insert(idx);
+            for(auto& neigh: adj[idx]) {
+                if(--indeg[neigh] == 0) {
+                    q.push(neigh);
+                }
+            }
+        }
+
+        return (vis.size() == n);
     }
 };
