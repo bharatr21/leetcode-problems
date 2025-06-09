d0125bbb5bf5fed0fa1e70be4a143b1393f12568
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-06-08 23:54:58 -0400

    Time: 0 ms (100%), Space: 18.2 MB (67.22%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> res, emp;
         int n = numCourses;
         vector<vector<int>> adj(n);
-        vector<int> indeg(n);
+        vector<int> res, emp, indeg(n, 0);
+        vector<bool> vis(n, false);
         for(vector<int>& e: prerequisites) {
             int a = e[0], b = e[1];
             adj[b].push_back(a);
             indeg[a]++;
         }
         queue<int> q;
-        vector<bool> vis(n, false);
         for(int i = 0; i < n; i++) {
             if(indeg[i] == 0) {
                 q.push(i);
             }
         }
         while(!q.empty()) {
-            int c = q.front();
+            auto idx = q.front();
             q.pop();
-            res.push_back(c);
-            if(vis[c]) continue;
-            else {
-                vis[c] = true;
-                for(auto& neigh: adj[c]) {
-                    if(!vis[neigh] && --indeg[neigh] == 0) q.push(neigh);
-                }
+            vis[idx] = true;
+            res.push_back(idx);
+            for(auto& neigh: adj[idx]) {
+                if(!vis[neigh] && --indeg[neigh] == 0) q.push(neigh);
             }
         }
-        return ((res.size() == n) ? res : emp);
+        return (res.size() == n) ? res: emp;
     }
 };
