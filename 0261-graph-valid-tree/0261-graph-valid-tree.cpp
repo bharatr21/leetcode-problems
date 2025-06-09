d54f4c8078a4cfd09bda06b58dcee0dfd7c63567
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-06-08 23:16:36 -0400

    Time: 9 ms (16.6%), Space: 18.8 MB (16.53%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    void dfs(int u, vector<vector<int>>& adj, vector<int>& vis, vector<int>& parent, bool& flag)
-    {
-        vis[u] = 1;
-        for(auto v: adj[u])
-        {
-            if(vis[v] && parent[u] != v && parent[u] != -1) {flag = false; break;}
-            else if(!vis[v])
-            {
-                parent[v] = u;
-                dfs(v, adj, vis, parent, flag);
+    bool cycle = false;
+    void dfs(int idx, vector<vector<int>>& adj, unordered_set<int>& vis, vector<int>& parent) {
+        vis.insert(idx);
+        for(auto& neigh: adj[idx]) {
+            if(!vis.count(neigh)) {
+                parent[neigh] = idx;
+                dfs(neigh, adj, vis, parent);
+            } else if(parent[idx] != neigh && parent[idx] != -1) {
+                cycle = true;
+                break;
             }
         }
     }
+
     bool validTree(int n, vector<vector<int>>& edges) {
-        int i, ct = 0;
-        vector<vector<int>> adj(n, vector<int>());
-        vector<int> vis(n, 0);
+        vector<vector<int>> adj(n);
+        unordered_set<int> vis;
         vector<int> parent(n, -1);
-        for(vector<int> v: edges)
-        {
-            int a = v[0], b = v[1];
+        for(vector<int>& e: edges) {
+            int a = e[0], b = e[1];
             adj[a].push_back(b);
             adj[b].push_back(a);
         }
-        bool flag = true;
-        for(int i = 0; i < n; i++)
-        {
-            if(!vis[i])
-            {
-                if(ct > 0) return false;
-                ct++;
-                dfs(i, adj, vis, parent, flag);
-            }
-        }
-        return flag;
+        dfs(0, adj, vis, parent);
+        return (vis.size() == n && !cycle);
     }
 };
