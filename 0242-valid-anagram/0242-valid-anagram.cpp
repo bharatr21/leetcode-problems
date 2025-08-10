b9023882cb0e4764f70c8e270462c37b26810481
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-08-10 16:42:23 -0500

    Time: 3 ms (51.68%), Space: 9.9 MB (26.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> fs, ft;
-        for(char c: s) fs[c]++;
-        for(char c: t) ft[c]++;
-        for(auto u: fs)
-        {
-            if(ft.find(u.first) == ft.end() || ft[u.first] != u.second) return false;
-        }
-        for(auto u: ft)
-        {
-            if(fs.find(u.first) == fs.end() || fs[u.first] != u.second) return false;
+        if(s.size() != t.size()) return false;
+        int n = s.size();
+        unordered_map<char, int> fres, fret;
+        for(char ch: s) fres[ch]++;
+        for(char ch: t) fret[ch]++;
+        for(auto u: fres) {
+            if(u.second != fret[u.first]) return false;
         }
         return true;
     }
