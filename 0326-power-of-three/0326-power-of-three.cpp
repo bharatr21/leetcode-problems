d6651c6d62896798387d6cbce0681c2717347f7b
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-08-12 21:54:05 -0500

    Time: 3 ms (60.14%), Space: 8.9 MB (46.41%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -4,7 +4,7 @@ public:
         if(n <= 0) return false;
         int tmp = n;
         while(tmp > 1) {
-            if(tmp % 3 && tmp > 1) return false;
+            if(tmp % 3) return false;
             tmp = tmp / 3;
         }
         return (tmp == 1);
