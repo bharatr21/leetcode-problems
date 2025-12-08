932743be9703d4063a093f092bda7a92958858d4
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-12-08 16:49:27 -0600

    Time: 4 ms (77.23%), Space: 7.9 MB (20.85%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -4,9 +4,9 @@ public:
         int res = 0;
         double c = 0.0;
         for(int a = 1; a <= n; a++) {
-            for(int b = 1; b <= n; b++) {
+            for(int b = a+1; b <= n; b++) {
                 c = sqrt(a * a + b * b);
-                if(c == int(c) && c <= n) res++; 
+                if(c == int(c) && c <= n) res+=2; 
             }
         }
         return res;
