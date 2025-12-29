f732629afface8ee8e1b0ed65db928b369ec1c67
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-12-28 22:55:35 -0600

    Time: 0 ms (100%), Space: 26.9 MB (46.15%) - LeetHub

--- 1176-diet-plan-performance/1176-diet-plan-performance.cpp
+++ 1176-diet-plan-performance/1176-diet-plan-performance.cpp
@@ -3,13 +3,13 @@ public:
     int dietPlanPerformance(vector<int>& calories, int k, int lower, int upper) {
         int n = calories.size(), rsum = 0, points = 0;
         for(int i = 0; i < k; i++) rsum += calories[i];
-        if(rsum < lower) points--;
-        else if(rsum > upper) points++;
-        for(int i = 0; i < n - k; i++) {
-            rsum += (calories[i+k] - calories[i]);
+        for(int i = k; i < n; i++) {
             if(rsum < lower) points--;
             else if(rsum > upper) points++;
+            rsum += (calories[i] - calories[i-k]);
         }
+        if(rsum < lower) points--;
+        else if(rsum > upper) points++;
         return points;
     }
 };
