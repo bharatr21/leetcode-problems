7fa2d00644c41ca16ff58b4c004e482b1f579430
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2026-01-01 12:21:08 -0600

    Time: 7 ms (42.19%), Space: 18 MB (51.67%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void dfs(int idx, vector<vector<int>>& adj, vector<bool>& vis) {
-        vis[idx] = true;
-        for(auto& neigh: adj[idx]) {
-            if(!vis[neigh]) {
-                dfs(neigh, adj, vis);
-            }
+    void dfs(int v, vector<vector<int>>& adj, vector<bool>& vis) {
+        vis[v] = true;
+        for(auto& u: adj[v]) {
+            if(!vis[u]) dfs(u, adj, vis);
         }
     }
 
     int countComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        for(vector<int>& e: edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+        vector<vector<int>> adj(n, vector<int>());
+        for(auto& e: edges) {
+            int a = e[0], b = e[1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
         }
-        int res = 0;
         vector<bool> vis(n, false);
+        int res = 0;
         for(int i = 0; i < n; i++) {
             if(!vis[i]) {
                 res++;
