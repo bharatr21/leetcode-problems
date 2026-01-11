1e5db539c275cd739f854e8fc288180039191fd5
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2026-01-10 23:28:19 -0600

    Time: 2 ms (87.21%), Space: 16.1 MB (97.94%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.size() == 0) return 0;
+        if (matrix.empty()) return 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> left(n), right(n), height(n);
+        fill(right.begin(), right.end(), n);
         int maxarea = 0;
-        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), 0));
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++) {
-                if (matrix[i][j] == '1') {
-                    // compute the maximum width and update dp with it
-                    dp[i][j] = j == 0 ? 1 : dp[i][j - 1] + 1;
-                    int width = dp[i][j];
-                    // compute the maximum area rectangle with a lower right
-                    // corner at [i, j]
-                    for (int k = i; k >= 0; k--) {
-                        width = std::min(width, dp[k][j]);
-                        maxarea = std::max(maxarea, width * (i - k + 1));
-                    }
+        for (int i = 0; i < m; i++) {
+            int cur_left = 0, cur_right = n;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
+            }
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1')
+                    left[j] = max(left[j], cur_left);
+                else {
+                    left[j] = 0;
+                    cur_left = j + 1;
                 }
+            }
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1')
+                    right[j] = min(right[j], cur_right);
+                else {
+                    right[j] = n;
+                    cur_right = j;
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                maxarea = max(maxarea, (right[j] - left[j]) * height[j]);
             }
         }
         return maxarea;
