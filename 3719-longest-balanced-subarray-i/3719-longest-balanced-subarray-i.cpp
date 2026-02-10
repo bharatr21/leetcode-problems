3a36b416d35de9ec83a64dfb82b53f277765cded
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2026-02-09 21:34:50 -0600

    Time: 1571 ms (90.78%), Space: 427.1 MB (83.02%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,4 +1,4 @@
-auto __fast_io_atexit = []() { ios::sync_with_stdio(false); cin.tie(nullptr); std::atexit([]() { ofstream("display_runtime.txt") << "0"; }); return 0; }();
+auto __fast_io_atexit = []() { ios::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
 
 class Solution {
 public:
