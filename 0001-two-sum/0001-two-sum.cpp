68827bbcb59e92eac57f7055d53f5f9188b3316a
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2026-02-20 01:44:12 -0600

    Time: 0 ms (100%), Space: 14.8 MB (36.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int, int> mp;
         for(int i = 0; i < n; i++) {
             if(mp.count(target - nums[i]) && mp[target - nums[i]] != i) {
-                return {i, mp[target - nums[i]]};
+                return {mp[target - nums[i]], i};
             }
             mp[nums[i]] = i;
         }
