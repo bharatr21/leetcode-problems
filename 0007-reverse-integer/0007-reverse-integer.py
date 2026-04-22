2e4241075a697de8dfb0dd667c8c9198fef6fe35
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2026-04-22 00:19:04 -0500

    Time: 47 ms (50.9%), Space: 19.4 MB (12.27%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -7,8 +7,8 @@ class Solution:
         res = 0
         while tmp:
             digit = tmp % 10
-            if res == imax / 10 and ((sgn == 1 and digit > 7) or (sgn == -1 and digit > 8)): return 0
-            elif res > imax / 10: return 0
+            if res == imax // 10 and ((sgn == 1 and digit > 7) or (sgn == -1 and digit > 8)): return 0
+            elif res > imax // 10: return 0
             res = res * 10 + digit
             tmp //= 10
         return sgn * res
