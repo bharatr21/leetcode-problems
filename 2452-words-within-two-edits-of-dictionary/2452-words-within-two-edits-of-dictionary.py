7588232284d97a93612ae3dc96d6b36338d25c11
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2026-04-21 23:42:13 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -0,0 +1,13 @@
+def twoEdits(word1: str, word2: str, n: int) -> bool:
+    return sum([word1[i] != word2[i] for i in range(n)]) <= 2
+
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        n = len(queries[0])
+        res = []
+        for q in queries:
+            for word in dictionary:
+                if twoEdits(q, word, n):
+                    if q not in res:
+                        res.append(q)
+        return res
